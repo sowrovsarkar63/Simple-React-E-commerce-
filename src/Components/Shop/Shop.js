c84acf3e5423ae029df018863a73cd4d36d50645
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-09-25 04:12:33 +0600

    Basic shop layout completed

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -2,8 +2,14 @@ import React from "react";
 import "./Shop.css";
 function Shop() {
     return (
-        <div>
-            <h1>Hello from shop Components</h1>
+        <div className="shop-container">
+            <div className="product-container">
+                <h1>Product</h1>
+            </div>
+            <div className="cart-container">
+                <h1>Shoping cart </h1>
+                <h3>Total Cost </h3>
+            </div>
         </div>
     );
 }
