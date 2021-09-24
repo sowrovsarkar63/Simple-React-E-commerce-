7da917836f50cc289fffae3d78a77a8c54206217
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-09-25 00:37:08 +0600

    Started working on this project dedicatedly

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Header from "./Components/Header/Header";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1>Hello world</h1>
+                <Header></Header>
             </header>
         </div>
     );
