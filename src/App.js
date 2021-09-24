10354c68a74e7dba276f4188af3adc0f4e72f966
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-09-25 03:33:00 +0600

    Shop component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
+import Shop from "./Components/Shop/Shop";
 
 function App() {
     return (
         <>
             <header className="app-header">
                 <Header></Header>
+                <Shop></Shop>
             </header>
         </>
     );
---
+++ src/Components/Shop/Shop.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Shop.css";
+function Shop() {
+    return (
+        <div>
+            <h1>Hello from shop Components</h1>
+        </div>
+    );
+}
+export default Shop;
