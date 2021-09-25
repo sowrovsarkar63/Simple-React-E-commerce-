2fbcc0031ebf811278ad5dcf68c317dde5389d25
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-09-25 06:38:45 +0600

    basic shoping cart system completed

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -3,15 +3,26 @@ import React from "react";
 function Cart(props) {
     const { cart } = props;
     let total = 0;
+
     for (const product of cart) {
         total = total + product.price;
     }
+    const shiping = 15;
+    const tax = (total + shiping) * 0.1;
+    const grandTotal = total + shiping + tax;
     return (
         <div>
             <h1>Order summary</h1>
             <h3>Items Ordered {props.cart.length}</h3>
+
             <br />
-            <p>Total: {total}</p>
+            <p>Total: {total.toFixed(2)}</p>
+            <br />
+            <p>Shiping {shiping}</p>
+            <br />
+            <p>Taxt {tax.toFixed(2)}</p>
+
+            <h5>Grand Total {grandTotal.toFixed(2)}</h5>
         </div>
     );
 }
